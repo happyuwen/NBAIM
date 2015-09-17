8e7a000a6c277854de298bae50187af83f744873
Yuwen <happyuwen@gmail.com>
2015-09-17 11:01:05 +0800

    Update draw_CDF.R

--- plot/draw_CDF.R
+++ plot/draw_CDF.R
@@ -1,3 +1,4 @@
+library('ggplot2')
 data<-read.table(file = "/Users/happyuwen/Google\ 雲端硬碟/HTC_G2/Data/GeoText/experiment_v2/geotext_acc.txt",col.names=c("ID","Freq","NB","SVM","NMF","CLAR","NBAIM","n"),quote="",sep=",",stringsAsFactors=F)
 
 NB<-data$NB
